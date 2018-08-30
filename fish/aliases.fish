9e22347a8a5a5dae31d8d3ad04cbfe99e7affb6d
satoship <satoship@gmail.com>
2018-08-30 16:31:45 +0900

    Add fish

---
+++ fish/aliases.fish
@@ -0,0 +1,5 @@
+# https://github.com/paulirish/dotfiles/blob/master/fish/aliases.fish
+
+# shortcuts
+alias g="git"
+
---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+status --is-interactive; and source (rbenv init -|psub)
+
+# https://github.com/paulirish/dotfiles/blob/master/fish/config.fish
+source ~/.config/fish/aliases.fish
+
